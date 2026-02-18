9390a1d46ab60b7bfd3dacbb330b0e33102b6e1e
Wladislaw Repin <repin@mailbox.org>
2026-02-18 12:48:06 +0100

    chore(firebase): add project and platform setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:yamt/app.dart';
+import 'package:yamt/core/config/firebase_config.dart';
 
-void main() {
-  runApp(const MainApp());
-}
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
-  }
+  await setupFirebase();
+  runApp(ProviderScope(child: const YAMT()));
 }
